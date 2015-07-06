e41e10fae1a1ac3a75706cae8652024c8ba116a9
acaffarelli <adicaff@hotmail.com>
2015-07-06 18:09:27 -0300

    fix in user model at active admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register User do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
+  permit_params :email, :first_name, :last_name, :username
   #
   # or
   #
@@ -13,6 +13,58 @@ ActiveAdmin.register User do
   #   permitted << :other if resource.something?
   #   permitted
   # end
+
+  form do |f|
+    inputs 'Details' do
+      input :email
+      input :first_name
+      input :last_name
+      input :username
+    end
+
+    actions
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :first_name
+    column :last_name
+    column :username
+    column :sign_in_count
+    column :created_at
+    column :updated_at
+    column :facebook_id
+
+    actions
+  end
+
+  filter :id
+  filter :email
+  filter :username
+  filter :first_name
+  filter :last_name
+  filter :sign_in_count
+  filter :created_at
+  filter :updated_at
+  filter :facebook_id
+
+
+  show do
+    attributes_table do
+      row :id
+      row :email
+      row :first_name
+      row :last_name
+      row :username
+      row :sign_in_count
+      row :created_at
+      row :updated_at
+      row :facebook_id
+    end
+    active_admin_comments
+  end
 
 
 end
