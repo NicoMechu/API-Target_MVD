87073dad6004c0945b8870c5d5cd22ae8c33e52b
Demian Ardus <demian@toptierlabs.com>
2015-11-25 13:20:29 -0200

    added put to rack cors permitted methods

--- config/initializers/rack_cors.rb
+++ config/initializers/rack_cors.rb
@@ -3,7 +3,7 @@ module Railsroot
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options, :put]
       end
     end
   end
