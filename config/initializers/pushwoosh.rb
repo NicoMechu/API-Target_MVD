2776d66ab7c8e3e1ee2efb14b285cead54a78dc9
Nicolas Mechulam <nicolas@toptierlabs.com>
2016-09-15 13:55:28 -0300

    Pushwoosh credentials changed

--- config/initializers/pushwoosh.rb
+++ config/initializers/pushwoosh.rb
@@ -1,4 +1,4 @@
 Pushwoosh.configure do |config|
-  config.application = '49D85-1997B'
-  config.auth = 'pdEy8YWFfuVWzafe0gu9XTJHlLmTv5dMgLriGKe8b1VAw6Nsjfb97d0XemnAQRiODYuiVCWqFbVLmWwLLA75'
+  config.application = ENV['PUSHWOOSH_APPLICATION']
+  config.auth = ENV['PUSHWOOSH_AUTH']
 end
